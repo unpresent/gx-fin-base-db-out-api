d2c33bf76c15e4020f61204e0fb502b8e57792f7
Vladimir Gagarkin <vladimir.gagarkin@gmail.com>
2021-12-08 23:35:14 +0300

    v1.2.3. ChannelsNames -> DrisChannelsNames.

--- src/main/java/ru/gx/fin/common/dris/config/DrisChannelsNames.java
+++ src/main/java/ru/gx/fin/common/dris/config/DrisChannelsNames.java
@@ -1,8 +1,8 @@
 package ru.gx.fin.common.dris.config;
 
 @SuppressWarnings("unused")
-public abstract class ChannelsNames {
-    public abstract static class DrisSnapshots {
+public abstract class DrisChannelsNames {
+    public abstract static class Snapshots {
         public static final String PLACES = "dris-snapshots.places";
         public static final String PROVIDER_TYPES = "dris-snapshots.provider-types";
         public static final String PROVIDERS = "dris-snapshots.providers";
